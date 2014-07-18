31f5a06cf4d15b6c36cae8a19eaa9bbd5caa75e6
hissan1983 <hissan1983@gmail.com>
2014-07-18 17:19:33 +0900

    ショップカテゴリ、プレイ内容マスタをDBマイグレーション時に入れられるようにseeds.rb編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ShopCategory.create(:category => 'ソープ')
+ShopCategory.create(:category => 'ヘルス')
+ShopCategory.create(:category => 'デリヘル')
+ShopCategory.create(:category => 'イメクラ')
+
+#ShopInfo.create(:address => '大阪府大阪市', :url => 'http://cocoshop.jp', :lat => 34.722665, :lon => 135.41869, :tel => 06-111-1111, :min_price => 10000, :max_price => 13000, :shop_category => 1)
+
+PlayKind.create(:kind => 'フェラ')
+PlayKind.create(:kind => '素股')
+PlayKind.create(:kind => '手コキ')
