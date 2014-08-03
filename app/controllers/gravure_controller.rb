76d026ab4f96fdf54a1317a6889125e6c4ba13e4
hiroyuki <hissan1983@gmail.com>
2014-08-03 10:49:36 +0900

    グラビアページでキャストを中心にショップ情報をネストするよう変更

--- app/controllers/gravure_controller.rb
+++ app/controllers/gravure_controller.rb
@@ -18,13 +18,22 @@ class GravureController < ApplicationController
     min_lat = params[:lat].to_f - 50
     max_lng = params[:lng].to_f + 100
     min_lng = params[:lng].to_f - 100
-    @shop_infos = ShopInfo.where('lat <= ? AND lat >= ? AND lng <= ? AND lng >= ?',
-      max_lat, min_lat, max_lng, min_lng)
-
-    render json: @shop_infos.to_json(:only => ['name', 'lat', 'lng', 'tel'],
+    # @shop_infos = ShopInfo.where('lat <= ? AND lat >= ? AND lng <= ? AND lng >= ?',
+    #   max_lat, min_lat, max_lng, min_lng)
+    @casts = Cast.joins(:shop_info).where(
+      'lat <= ? AND lat >= ? AND lng <= ? AND lng >= ? AND free_after <= ? AND free_after >= ?',
+      max_lat, min_lat, max_lng, min_lng, 60.minutes.from_now, 30.minutes.ago
+      )
+    render json: @casts.to_json(:only => ['id', 'name', 'free_after'],
       :include => {
-        :casts => {:only => ['id', 'name']}
-        })
+        :shop_info =>{:only => ['id', 'name', 'lat', 'lng', 'tel']}
+      })
+
+
+    # render json: @shop_infos.to_json(:only => ['name', 'lat', 'lng', 'tel'],
+    #   :include => {
+    #     :casts => {:only => ['id', 'name']}
+    #     })
   end
 
   private
